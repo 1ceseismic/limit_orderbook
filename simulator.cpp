5df50bed50a1ab421884492053366ebf69a66802
joeru <joeru.dev@gmail.com>
2025-08-18 20:32:54 +1200

    correct output with final orderbook status, for but overall non-idiomatic

--- simulator.cpp
+++ simulator.cpp
@@ -8,7 +8,7 @@ struct Order
 {
     uint64_t id;
     int client_id;
-    int book_id;
+    short book_id;
     uint64_t token;
     bool is_buy;
     uint32_t quantity;
@@ -42,55 +42,107 @@ struct OrderBook
     std::map<int, std::deque<Order*>, std::less<int>> asks;
     
 
-    void add_order(Order order, auto& opp){
+    void add_order(Order order) {
 
         orders.push_back(order);
         Order* o = &orders.back();
         id_index[o->id] = o;
-        bool is_buy = o->is_buy;
-        uint32_t total_qty;
+        token_to_order[o->token] = o;
+    
+        if (o->is_buy) {
+            auto& opp = asks;
+            
+            while (o->quantity > 0 && !opp.empty()) {
+                auto best_it = opp.begin();
+                int best_price = best_it->first;
+    
+                if (o->price < best_price) break; //no match
+    
+                auto& q = best_it->second; //the queue/price level of orders for this price
+                int initial_quantity = o->quantity;
+                
+                while (!q.empty() && o->quantity > 0) {
+                    Order* m = q.front();
+                    if (m->quantity <= 0) {
+                        q.pop_front();
+                        id_index.erase(m->id);
+                        token_to_order.erase(m->token);
+                        continue;
+                    }
+                    
+                    int traded = std::min(o->quantity, m->quantity);
 
-        
-        //now we try matching
-        while (o->quantity > 0 && !opp.empty()){
-            auto best_it  = opp.begin();
-            int best_price = best_it->first;
-            if ((is_buy && o->price < best_price) || !is_buy && o->price > best_price) break;  //no match
-
-            //loop through quantities to fullfill bid
-            auto& q = best_it ->second; //q is queue/level of orders for that price 
-            while(!q.empty() && o->quantity > 0){
-
-                Order* m = q.front();
-                if (!(m->quantity > 0)) {
-                    q.pop_front(); 
-                    id_index.erase(m->id); 
-                    continue;
+                    printf("E, Client %d, Token %ld, %d, %d\n", m->client_id, m->token, traded, best_price);
+    
+                    o->quantity -= traded;
+                    m->quantity -= traded;
+    
+                    if (m->quantity == 0) {
+                        q.pop_front();
+                        id_index.erase(m->id);
+                        token_to_order.erase(m->token);
+                    }
                 }
-
-                int traded = std::min(o->quantity, m->quantity);
-                o->quantity -=traded;
-                m->quantity -= traded;
-                printf("E, Client %d, Token %d, %d, %d\n", m->client_id, m->token, traded, best_price); //resting order
-
-                if (m->quantity ==0) {
-                    q.pop_front();
-                    id_index.erase(m->id);
+                if (initial_quantity > o->quantity) {
+                    int total_traded = initial_quantity - o->quantity;
+                    printf("E, Client %d, Token %ld, %d, %d\n", o->client_id, o->token, total_traded, best_price);
                 }
-
-                printf("E, Client %d, Token %ld, %d, %d\n", o->client_id, o->token, traded, best_price);
-
             }
-
-        }
-
-        //leftover for book
-        if (o->quantity > 0){
-            if (is_buy) bids[o->price].push_back(o);
-            else asks[o->price].push_back(o);
-        }  
-        else{
-            id_index.erase(o->id); 
+            
+            if (o->quantity > 0) {
+                bids[o->price].push_back(o);
+            } else {
+                id_index.erase(o->id);
+                token_to_order.erase(o->token);
+            }
+            
+        } else { //handle sell, match against bids 
+            auto& opp = bids;
+    
+            while (o->quantity > 0 && !opp.empty()) {
+                auto best_it = opp.begin();
+                int best_price = best_it->first;
+    
+                if (o->price > best_price) break; // No match
+                
+                auto& q = best_it->second;
+                int initial_quantity = o->quantity;
+    
+                while (!q.empty() && o->quantity > 0) {
+                    Order* m = q.front();
+                    if (m->quantity <= 0) {
+                        q.pop_front();
+                        id_index.erase(m->id);
+                        token_to_order.erase(m->token);
+                        continue;
+                    }
+                    int traded = std::min(o->quantity, m->quantity);
+                    
+                    printf("E, Client %d, Token %ld, %d, %d\n", m->client_id, m->token, traded, best_price); //resting order
+    
+                    o->quantity -= traded;
+                    m->quantity -= traded;
+                    
+                    if (m->quantity == 0) {
+                        q.pop_front();
+                        id_index.erase(m->id);
+                        token_to_order.erase(m->token);
+                    }
+                }
+                //after all trades at this price ; print incoming order
+                if (initial_quantity > o->quantity) {
+                    int total_traded = initial_quantity - o->quantity;
+                    printf("E, Client %d, Token %ld, %d, %d\n", o->client_id, o->token, total_traded, best_price);
+                }
+            }
+    
+            //leftover for book
+            if (o->quantity > 0) {
+                asks[o->price].push_back(o);
+            } else { //fully executed so we can rm
+                id_index.erase(o->id);
+                token_to_order.erase(o->token);
+            }
         }
     }
     
@@ -129,11 +181,11 @@ void parseOB(std::istream& input, OrderBook& ob){
         if (tokens.empty()) continue;
         if (tokens[0]  == "O" && tokens.size() == 7){
             string book_name = tokens[2];
-
             OrderBook& ob = all_books[book_name];
             Order order{
                 .id =  next_id++,
                 .client_id = extractId(tokens[1]),
+                .book_id = extractId(book_name),
                 .token = extractId(tokens[3]),
                 .is_buy = (tokens[4] == "B"),
                 .quantity =std::stoul(tokens[5]),
@@ -144,9 +196,9 @@ void parseOB(std::istream& input, OrderBook& ob){
             token_to_order[order.token] = &order;
 
             if (order.is_buy) {
-                ob.add_order(order, ob.asks); 
+                ob.add_order(order); 
             } else { 
-                ob.add_order(order, ob.bids); 
+                ob.add_order(order); 
             }
 
         } else if ((tokens[0] == "X" || tokens[0] == "C") && tokens.size() ==3){
@@ -173,10 +225,31 @@ void parseOB(std::istream& input, OrderBook& ob){
 int main(){
     OrderBook ob;
     std::ifstream infile("input_orders.txt");
-    if (!infile) {std::cerr << "cant open input file\n"; return 1;}
+    if (!infile) {std::cerr << "cant open  file\n"; return 1;}
 
     parseOB(infile, ob);
-    std::cout <<"O: {order book status} \n";
+    std::cout <<"\n";
     
+    for (auto& [bname, _ob] :  all_books){
+        for (const auto& pricelevel : _ob.bids){
+
+            for (Order* o : pricelevel.second){
+                if (o->quantity > 0) {//not fully executed
+                    char type = o->is_buy ? 'B' : 'S';
+                    printf("O, Client %d, %s, Token %ld, %c, %ld, %ld \n", o->client_id, o->book_id, o->token, type, o->quantity, o->price);
+                }
+            }
+        }
+
+        for (const auto& pricelevel : _ob.asks){
+            for (Order* o : pricelevel.second){
+                if (o->quantity > 0) {//not fully executed
+                    char type = o->is_buy ? 'B' : 'S';
+                    printf("O, Client %d, Orderbook %d, Token %ld, %c, %ld, %ld \n", o->client_id, o->book_id, o->token, type, o->quantity, o->price);
+                }
+            }
+        }
+
+    }
     return 0;
 }
